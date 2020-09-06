d5e77997518edf356ec53307a3339db7d28fca16
Bikash Mishra <bikashmishra.developer@gmail.com>
2020-09-06 17:24:48 -0400

    data in local variable

--- src/app/node-api/node-user.service.ts
+++ src/app/node-api/node-user.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClientService } from '../services/http-client.service';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NodeUserService {
 
+  private userList;
+
   private url = 'http://localhost:3000/api/users';
 
   constructor(private httpClientService: HttpClientService) { }
 
-  get usersFromNodeApi$() { return this.httpClientService.getProtectedApi(this.url); }
+  get usersFromNodeApi$() {
+    if (this.userList) {
+      return of(this.userList);
+    }
+
+    let users = this.httpClientService.getProtectedApi(this.url)
+    users.subscribe((users: any) => this.userList = users);
+    return users;
+  }
 
 }
